ff5e7d825264f50e2cff4ce909489b0bec724b28
zayennn <zaayeennn@gmail.com>
2025-11-29 21:58:45 +0700

    initialized flask on project

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print('hello world')
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index() :
+    datas = ['fulan', 'fulani', 'fulano']
+    return render_template('index.html', datas=datas)
+
+if __name__ == "__main__" :
+    app.run(debug=True)
