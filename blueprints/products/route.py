15c7debe50b9f3e78383a892ada2a7c5a85e8e57
zayennn <zaayeennn@gmail.com>
2025-12-01 13:07:52 +0700

    created page create product

--- blueprints/products/route.py
+++ blueprints/products/route.py
@@ -1,7 +1,11 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 products_bp = Blueprint('products', __name__, url_prefix='/products')
 
 @products_bp.route('/')
 def index() :
     return render_template('pages/products/index.html')
+
+@products_bp.route('/create-product')
+def create() :
+    return render_template('pages/products/create.html')
