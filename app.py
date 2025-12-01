be29d9bbb74dd212b6d5a60a0fc600f53ab94b55
zayennn <zaayeennn@gmail.com>
2025-12-01 12:00:08 +0700

    initialized flask-mysqldb on app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from flask_mysqldb import MySQL
 
 # import blueprints
 from blueprints.home.route import home_bp
@@ -10,6 +11,15 @@ app = Flask(__name__)
 app.register_blueprint(home_bp)
 app.register_blueprint(products_bp)
 app.register_blueprint(history_bp)
+
+
+# flask mysql db
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'db_project_uts'
+
+mysql = MySQL(app)
 
 if __name__ == "__main__" :
     app.run(debug=True)
